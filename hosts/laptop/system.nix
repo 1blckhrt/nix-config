80698934ade56a13308612df420792bcb58979f8
1blckhrt <williams.1691@wright.edu>
2025-09-23 07:15:25 -0400

    refactor(laptop-host): remove tailscale

--- hosts/laptop/system.nix
+++ hosts/laptop/system.nix
@@ -12,26 +12,4 @@
 
   system-graphics.enable = true;
   system-graphics.enable32Bit = true;
-
-  systemd.services.tailscale = {
-    enable = true;
-  };
-
-  systemd.services.tailscaled = {
-    description = "Tailscale node agent";
-    after = ["network.target"];
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      ExecStart = "/run/system-manager/sw/bin/tailscaled --state=/var/lib/tailscale/tailscaled.state";
-      Restart = "on-failure";
-      User = "root";
-      CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
-      AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
-      NoNewPrivileges = true;
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    tailscale
-  ];
 }
