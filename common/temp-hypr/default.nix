eefb4eae65a34f4bb9ffe5da434cbfdef28bfff2
1blckhrt <143528734+1blckhrt@users.noreply.github.com>
2025-11-13 19:41:27 -0500

    refactor(wm): change dir structure

--- common/temp-hypr/default.nix
+++ common/temp-hypr/default.nix
@@ -1,23 +1,5 @@
 {
-  pkgs,
-  config,
-  ...
-}: {
-  xdg.configFile = let
-    mkSymlink = config.lib.file.mkOutOfStoreSymlink;
-    confPath = "${config.home.homeDirectory}/nix-config/common/temp-hypr/conf";
-  in {
-    "hypr/configs".source = mkSymlink confPath;
-  };
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      source = let
-        configPath = "${config.home.homeDirectory}/.config/hypr/configs";
-      in [
-        "${configPath}/main.conf"
-      ];
-    };
-  };
+  imports = [
+    ./wm/default.nix
+  ];
 }
---
+++ common/temp-hypr/wm/default.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  xdg.configFile = let
+    mkSymlink = config.lib.file.mkOutOfStoreSymlink;
+    confPath = "${config.home.homeDirectory}/nix-config/common/temp-hypr/wm/conf";
+  in {
+    "hypr/configs".source = mkSymlink confPath;
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      source = let
+        configPath = "${config.home.homeDirectory}/.config/hypr/configs";
+      in [
+        "${configPath}/main.conf"
+      ];
+    };
+  };
+}
