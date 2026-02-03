41af9c98ba7811039ae9193167614ce18fb4b9fd
1blckhrt <blckhrtxo@gmail.com>
2026-02-03 10:44:35 -0500

    chore: backup on 2026-02-03 10:44:35

--- common/terminal/tmux.nix
+++ common/terminal/tmux.nix
@@ -3,22 +3,24 @@
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "tmux-256color";
-    baseIndex = 1;
     prefix = "C-x";
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
       {
-        plugin = nord.overrideAttrs (old: {
-          postInstall =
-            (old.postInstall or "")
-            + ''
-              # Ensure the script has a proper bash shebang
-              if ! head -n1 $out/share/tmux-plugins/nord/nord.tmux | grep -q '^#!/usr/bin/env bash'; then
-                sed -i '1s|^#!.*|#!/usr/bin/env bash|; t; 1s|^|#!/usr/bin/env bash\n|' \
-                  $out/share/tmux-plugins/nord/nord.tmux
-              fi
-            '';
-        });
+        plugin = dotbar;
+        extraConfig = ''
+          set -g @tmux-dotbar-right true
+          set -g @tmux-dotbar-position top
+          set -g @tmux-dotbar-ssh-icon '󰌘'
+          set -g @tmux-dotbar-ssh-icon-only false
+          set -g @tmux-dotbar-ssh-enabled true
+          # nord theme
+          set -g @tmux-dotbar-bg "#2e3440"
+          set -g @tmux-dotbar-fg "#4c566a"
+          set -g @tmux-dotbar-fg-current "#eceff4"
+          set -g @tmux-dotbar-fg-session "#d8dee9"
+          set -g @tmux-dotbar-fg-prefix "#b48ead"
+        '';
       }
       {
         plugin = resurrect;
@@ -40,31 +42,30 @@
     ];
 
     extraConfig = ''
-      bind r source-file ~/.tmux.conf \; display-message "Config reloaded!"
-      set -g set-clipboard on
-      set -g detach-on-destroy off
-      set -g escape-time 0
-      set -g history-limit 1000000
-      set -g mouse on
-      set -g status-interval 3
-      set -g allow-passthrough on
-      set -g status-position top
-      set -g pane-base-index 1
-      set -g renumber-windows on
+          bind r source-file ~/.tmux.conf \; display-message "Config reloaded!"
+          set -g set-clipboard on
+          set -g detach-on-destroy off
+          set -g escape-time 0
+          set -g history-limit 1000000
+          set -g mouse on
+          set -g status-interval 3
+          set -g allow-passthrough on
 
-      unbind %
-      bind | split-window -h -c "#{pane_current_path}"
-      unbind '"'
-      bind - split-window -v -c "#{pane_current_path}"
-      bind c new-window -c "#{pane_current_path}"
+      set-option -g renumber-windows on
+          set -g base-index 1
+      setw -g pane-base-index 1
 
-      bind -r j resize-pane -D 5
-      bind -r k resize-pane -U 5
-      bind -r h resize-pane -L 5
-      bind -r l resize-pane -R 5
-      bind -r m resize-pane -Z
+          unbind %
+          bind | split-window -h -c "#{pane_current_path}"
+          unbind '"'
+          bind - split-window -v -c "#{pane_current_path}"
+          bind c new-window -c "#{pane_current_path}"
 
-      set -g status-position top
+          bind -r j resize-pane -D 5
+          bind -r k resize-pane -U 5
+          bind -r h resize-pane -L 5
+          bind -r l resize-pane -R 5
+          bind -r m resize-pane -Z
     '';
   };
 }
