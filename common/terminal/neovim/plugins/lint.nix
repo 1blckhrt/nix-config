5039e537e111d4dcb17828638e7c3f67cf47c149
1blckhrt <blckhrtxo@gmail.com>
2026-02-04 00:58:56 -0500

    chore: backup on 2026-02-04 00:58:56

--- common/terminal/neovim/plugins/lint.nix
+++ common/terminal/neovim/plugins/lint.nix
@@ -1,8 +1,24 @@
 _: {
-  programs.nixvim.plugins.lint = {
-    enable = true;
-    lintersByFt = {
-      "python" = ["mypy"];
+  programs.nixvim = {
+    plugins.lint = {
+      enable = true;
+      lintersByFt = {
+        python = ["mypy"];
+      };
+      linters.mypy.args = [
+        "--pretty"
+      ];
     };
+
+    autoCmd = [
+      {
+        event = ["BufWritePost" "BufEnter" "InsertLeave"];
+        callback.__raw = ''
+          function()
+            require("lint").try_lint()
+          end
+        '';
+      }
+    ];
   };
 }
